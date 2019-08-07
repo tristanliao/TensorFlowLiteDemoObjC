9beeab1306ef78d9ed82dba820db9d47f5f9ef35
TristanLiao <tristan@17.media>
2019-08-07 21:53:51 +0800

    Use camera when starting the app.

--- TensorFlowLiteDemoObjC/ViewController.m
+++ TensorFlowLiteDemoObjC/ViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
+
+@property (strong, nonatomic) AVCaptureSession *session;
+@property (strong, nonatomic) AVCaptureDevice *inputDevice;
+@property (strong, nonatomic) AVCaptureDeviceInput *deviceInput;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *previewLayer;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fpsLabel;
 
 @end
 
@@ -16,8 +26,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setupCamera];
 }
 
+- (void)setupCamera {
+    self.session = [[AVCaptureSession alloc] init];
+    [self.session setSessionPreset:AVCaptureSessionPresetPhoto];
+    
+    self.inputDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error;
+    self.deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:self.inputDevice error:&error];
+    
+    if ([self.session canAddInput:self.deviceInput]) {
+        [self.session addInput:self.deviceInput];
+    }
+    
+    self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+    [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    CALayer *rootLayer = [[self view] layer];
+    [rootLayer setMasksToBounds:YES];
+    CGRect frame = self.view.frame;
+    [self.previewLayer setFrame:frame];
+    [rootLayer insertSublayer:self.previewLayer atIndex:0];
+    
+    AVCaptureVideoDataOutput *videoDataOutput = [AVCaptureVideoDataOutput new];
+    
+    NSDictionary *rgbOutputSettings = [NSDictionary
+                                       dictionaryWithObject:[NSNumber numberWithInt:kCMPixelFormat_32BGRA]
+                                       forKey:(id)kCVPixelBufferPixelFormatTypeKey];
+    [videoDataOutput setVideoSettings:rgbOutputSettings];
+    [videoDataOutput setAlwaysDiscardsLateVideoFrames:YES];
+    dispatch_queue_t videoDataOutputQueue = dispatch_queue_create("VideoDataOutputQueue", DISPATCH_QUEUE_SERIAL);
+    [videoDataOutput setSampleBufferDelegate:self queue:videoDataOutputQueue];
+    
+    if ([self.session canAddOutput:videoDataOutput])
+        [self.session addOutput:videoDataOutput];
+    [[videoDataOutput connectionWithMediaType:AVMediaTypeVideo] setEnabled:YES];
+    
+    [self.session startRunning];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+    CVImageBufferRef cvImage = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CIImage *ciImage = [[CIImage alloc] initWithCVPixelBuffer:cvImage];
+//    [self labelImage: ciImage];
+}
 
 @end
